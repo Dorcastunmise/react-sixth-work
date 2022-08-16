efe8e79e7e3ca21d834b1baebe3609c76589c150
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-08-16 20:14:02 +0100

    random app with react

---
+++ src/component/box.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Box(props) {
+    const styles = {
+        backgroundColor: props.on ? "#222222" : "transparent"
+    }
+    
+    return (
+        <div 
+            style={styles} 
+            className="box"
+            onClick={props.toggle}
+        >
+        </div>
+    )
+}
+export default Box
